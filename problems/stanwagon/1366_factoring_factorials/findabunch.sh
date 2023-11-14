f6d3b7acbe5df5456d62981785d8180f36e2bff9
Kate Novak <kje@cochleoid.com>
2023-11-14 08:16:26 -0500

    Problem of the Week 1366
    Adding a record of some factorizations I've found.

---
+++ problems/stanwagon/1366_factoring_factorials/findabunch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#( python3 trifactors.py {31..40} >> extra_31-40.txt ) &
+#( python3 trifactors.py {41..50} >> extra_41-50.txt ) &
+#( python3 trifactors.py {51..60} >> extra_51-60.txt ) &
+#( python3 trifactors.py {61..70} >> extra_61-70.txt ) &
+#( python3 trifactors.py {71..80} >> extra_71-80.txt ) &
+#( python3 trifactors.py {81..90} >> extra_81-90.txt ) &
+#( python3 trifactors.py {91..100} >> extra_91-100.txt ) &
+( python3 trifactors.py 101 >> extra_101.txt ) &
+( python3 trifactors.py 102 >> extra_102.txt ) &
+( python3 trifactors.py 103 >> extra_103.txt ) &
+( python3 trifactors.py 104 >> extra_104.txt ) &
+( python3 trifactors.py 105 >> extra_105.txt ) &
+( python3 trifactors.py 106 >> extra_106.txt ) &
+( python3 trifactors.py 107 >> extra_107.txt ) &
+( python3 trifactors.py 108 >> extra_108.txt ) &
+( python3 trifactors.py 109 >> extra_109.txt ) &
+( python3 trifactors.py 110 >> extra_110.txt ) &
+( python3 trifactors.py 111 >> extra_111.txt ) &
+( python3 trifactors.py 112 >> extra_112.txt ) &
+( python3 trifactors.py 113 >> extra_113.txt ) &
+( python3 trifactors.py 114 >> extra_114.txt ) &
+( python3 trifactors.py 115 >> extra_115.txt ) &
+( python3 trifactors.py 116 >> extra_116.txt ) &
