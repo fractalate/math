74393d4e7bcffd6a38a9a2c7bd932cd9819a48e2
Kate Novak <kje@cochleoid.com>
2024-04-23 16:46:15 -0400

    Lean Basic Set Theory Test

--- scratchpad/lean/LeanTest.lean
+++ scratchpad/lean/LeanTest.lean
@@ -1,6 +1,10 @@
 -- This module serves as the root of the `LeanTest` library.
 -- Import modules here that should be built as part of the library.
 import «LeanTest».Basic
+import Mathlib.Data.Set.Basic
 
 theorem one_equals_one : 1 = 1 := by
   rfl
+
+example (x : U) (A : Set U) (h : x ∈ A) : x ∈ A := by
+  exact h
